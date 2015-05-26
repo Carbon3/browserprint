747635aa662f05da5cf20c4976d6ea9ffe96ef5e
nothing <nothing@192-168-1-3.tpgi.com.au>
2015-05-26 21:15:48 +0930

    Added error handling to the fingerprinting functions

--- WebContent/fingerprint.js
+++ WebContent/fingerprint.js
@@ -7,47 +7,55 @@ function getPlatformFlash(flash){
 }
 
 function getPluginDetails() {
-	var plugins = "";
-
-	/*
-	 * This method said to not be supported by IE.
-	 */
-	for (var i = 0; i < navigator.plugins.length; ++i) {
-		var plugin = navigator.plugins[i];
-		plugins += "Plugin " + i + ": " + plugin.name + "; "
-				+ plugin.description + "; " + plugin.filename + ";";
-
-		for (var j = 0; j < plugin.length; ++j) {
-			plugins += " (" + plugin[j].description + "; " + plugin[j].type
-					+ "; " + plugin[j].suffixes + ")";
-		}
-		plugins += ". ";
-	}
-
-	if (plugins == "") {
+	try{
+		var plugins = "";
+	
 		/*
-		 * Try the method that works with IE. Uses an MIT licensed script,
-		 * PluginDetect.
+		 * This method said to not be supported by IE.
 		 */
-		var plugin_names = [ "Java", "QuickTime", "DevalVR", "Shockwave",
-				"Flash", "WindowsMediaplayer", "Silverlight", "VLC" ];
-		for (var i = 0; i < plugin_names.length; ++i) {
-			var version = PluginDetect.getVersion(plugin_names[i]);
-			if (version) {
-				plugins += plugin_names[i] + " " + version + "; ";
+		for (var i = 0; i < navigator.plugins.length; ++i) {
+			var plugin = navigator.plugins[i];
+			plugins += "Plugin " + i + ": " + plugin.name + "; "
+					+ plugin.description + "; " + plugin.filename + ";";
+	
+			for (var j = 0; j < plugin.length; ++j) {
+				plugins += " (" + plugin[j].description + "; " + plugin[j].type
+						+ "; " + plugin[j].suffixes + ")";
+			}
+			plugins += ". ";
+		}
+	
+		if (plugins == "") {
+			/*
+			 * Try the method that works with IE. Uses an MIT licensed script,
+			 * PluginDetect.
+			 */
+			var plugin_names = [ "Java", "QuickTime", "DevalVR", "Shockwave",
+					"Flash", "WindowsMediaplayer", "Silverlight", "VLC" ];
+			for (var i = 0; i < plugin_names.length; ++i) {
+				var version = PluginDetect.getVersion(plugin_names[i]);
+				if (version) {
+					plugins += plugin_names[i] + " " + version + "; ";
+				}
 			}
 		}
-	}
-	
-	if (plugins == ""){
-		plugins = "No plugins detected";
+		
+		if (plugins == ""){
+			plugins = "No plugins detected";
+		}
+	}catch(e){
+		return "Error";
 	}
 
 	return plugins;
 }
 
 function getScreenDetails() {
-	return screen.width + "x" + screen.height + "x" + screen.colorDepth;
+	try{
+		return screen.width + "x" + screen.height + "x" + screen.colorDepth;
+	}catch(e){
+		return "Error";
+	}
 }
 
 function getScreenDetailsFlash(flash){
@@ -59,60 +67,72 @@ function getFonts(flash){
 }
 
 function getTimeZone() {
-	return new Date().getTimezoneOffset();
+	try{
+		return new Date().getTimezoneOffset();
+	}catch(e){
+		return "Error";
+	}
 }
 
 function getSuperCookie() {
-	var test = "";
-
-	test += "DOM localStorage: ";
-	try {
-		if ('localStorage' in window && window['localStorage'] !== null) {
-			test += "Yes";
-		} else {
-			test += "No";
-		}
-	} catch (ex) {
-		test += "No";
-	}
-	test += ", ";
-
-	test += "DOM sessionStorage: ";
 	try{
-		if ('sessionStorage' in window && window['sessionStorage'] !== null) {
-			test += "Yes";
-		} else {
-			test += "No";
-		}
-	} catch (ex) {
-		test += "No";
-	}
-	test += ", ";
+		var test = "";
 	
-
-	test += "IE userData: ";
-	var persistDiv = $('<div id="tmpDiv" style="behavior:url(#default#userdata)"></div>');
-	persistDiv.appendTo(document.body);
-	try {
-		tmpDiv.setAttribute("remember", "original value");
-		tmpDiv.save("oXMLStore");
-		tmpDiv.setAttribute("remember", "overwritten");
-		tmpDiv.load("oXMLStore");
-		if ((tmpDiv.getAttribute("remember")) == "original value") {
-			test += "Yes";
-		} else {
+		test += "DOM localStorage: ";
+		try {
+			if ('localStorage' in window && window['localStorage'] !== null) {
+				test += "Yes";
+			} else {
+				test += "No";
+			}
+		} catch (ex) {
 			test += "No";
 		}
-	} catch (ex) {
-		test += "No";
+		test += ", ";
+	
+		test += "DOM sessionStorage: ";
+		try{
+			if ('sessionStorage' in window && window['sessionStorage'] !== null) {
+				test += "Yes";
+			} else {
+				test += "No";
+			}
+		} catch (ex) {
+			test += "No";
+		}
+		test += ", ";
+		
+	
+		test += "IE userData: ";
+		var persistDiv = $('<div id="tmpDiv" style="behavior:url(#default#userdata)"></div>');
+		persistDiv.appendTo(document.body);
+		try {
+			tmpDiv.setAttribute("remember", "original value");
+			tmpDiv.save("oXMLStore");
+			tmpDiv.setAttribute("remember", "overwritten");
+			tmpDiv.load("oXMLStore");
+			if ((tmpDiv.getAttribute("remember")) == "original value") {
+				test += "Yes";
+			} else {
+				test += "No";
+			}
+		} catch (ex) {
+			test += "No";
+		}
+	}catch(e){
+		return "Error";
 	}
 
 	return test;
 }
 
 function getTime(){
-	var time = new Date().getTime();
-	return time;
+	try{
+		var time = new Date().getTime();
+		return time;
+	}catch(e){
+		return "Error";
+	}
 }
 
 function getDateTime(){
@@ -125,33 +145,57 @@ function getDateTime(){
 }
 
 function getMathTan(){
-	return Math.tan(-1e300);
+	try{
+		return Math.tan(-1e300);
+	}catch(e){
+		return "Error";
+	}
 }
 
 function getAdsBlocked(){
-	if($('#ad').height() == 0){
-		//Ads are blocked.
-		return 1;
-	} else {
-		//Ads are not blocked.
-		return 0;
+	try{
+		if($('#ad').height() == 0){
+			//Ads are blocked.
+			return 1;
+		} else {
+			//Ads are not blocked.
+			return 0;
+		}
+	}catch(e){
+		return "Error";
 	}
 }
 
 function getCanvas(){
-	return canvasData;
+	try{
+		return canvasData;
+	}catch(e){
+		return "Error";
+	}
 }
 
 function getWebGL(){
-	return webGLData;
+	try{
+		return webGLData;
+	}catch(e){
+		return "Error";
+	}
 }
 
 function getWebGLVendor(){
-	return webGLVendor;
+	try{
+		return webGLVendor;
+	}catch(e){
+		return "Error";
+	}
 }
 
 function getWebGLRenderer(){
-	return webGLRenderer;
+	try{
+		return webGLRenderer;
+	}catch(e){
+		return "Error";
+	}
 }
 
 function getLanguageFlash(flash){
