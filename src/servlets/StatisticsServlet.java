4767a469a97845cd9518c11f2ddab43703b5f9c9
nothing <nothing@192-168-1-3.tpgi.com.au>
2015-06-23 21:06:59 +0930

    Created StatisticsServlet and pass requests for statistics through it

---
+++ src/servlets/StatisticsServlet.java
@@ -0,0 +1,40 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StatisticsServlet
+ */
+public class StatisticsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StatisticsServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*
+		 * Forward to the statistics page.
+		 */
+		request.getRequestDispatcher("/WEB-INF/statistics.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
